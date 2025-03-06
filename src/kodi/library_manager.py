336449db47ba6bb836b5bea180b10745f18e5ffe
jsaddiction <04reduramax@gmail.com>
2025-03-05 20:27:04 -0500

    Fix disabled hosts and notification disablement

--- src/kodi/library_manager.py
+++ src/kodi/library_manager.py
@@ -22,6 +22,10 @@ class LibraryManager:
         self.log.debug("Building list of Kodi Hosts")
         self.hosts: list[KodiRPC] = []
         for cfg in host_configs:
+            if not cfg.enabled:
+                self.log.info("Skipping disabled host %s", cfg.name)
+                continue
+
             if host := self._create_host(cfg, path_maps):
                 self.hosts.append(host)
             else:
@@ -35,6 +39,8 @@ class LibraryManager:
             port=cfg.port,
             user=cfg.user,
             password=cfg.password,
+            priority=cfg.priority,
+            disable_notifications=cfg.disable_notifications,
             path_maps=[{"radarr": x.radarr, "kodi": x.kodi} for x in path_maps],
         )
         self.log.debug("Testing connection with %s", cfg.name)
